524221b0f1c0eea2e8ff92f747fc601bbb102661
nomigarden <nomipotatosys@gmail.com>
2025-07-09 23:11:29 +0300

    fixed typo-bug with CORSMiddleware

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin=["*"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
